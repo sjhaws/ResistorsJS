18b56e3400cb84ee37294133c71bf60af485c598
Steven Haws <stevenjhaws@gmail.com>
2019-12-08 15:50:07 -0700

    added Return submit

--- index.js
+++ index.js
@@ -303,3 +303,15 @@ document.getElementById("exitGameButton").addEventListener("click", exitGame)
 document.getElementById("rulesButton").addEventListener("click", rules)
 document.getElementById("highScoreButton").addEventListener("click", scores)
 document.getElementById("recalculate").addEventListener("click", verifyScores)
+
+var input = document.getElementById("percentage");
+
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("next").click();
+  }
+});
